2b3a6ce7f4581c912f5d6344d0e3e9cbcc64709a
Yoany Stephany Mora Pérez <yoany.stephany@gmail.com>
2018-10-01 13:17:19 -0500

    my first transmission
    sending a simple message with the tx-rx modules

---
+++ rf_receiver/rf_receiver.ino
@@ -0,0 +1,20 @@
+#include <RH_ASK.h>
+#include <SPI.h>
+
+RH_ASK rf_driver;
+
+void setup() {
+  rf_driver.init();
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  uint8_t buf[23];
+  uint8_t buflen = sizeof(buf);
+  if(rf_driver.recv(buf, &buflen)){
+    Serial.print("Message Received: ");
+    Serial.println((char*)buf);  
+  }
+
+}
